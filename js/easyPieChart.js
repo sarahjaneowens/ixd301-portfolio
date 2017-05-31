92e9e99576f03d3121e435039c557138c90f3a37
Sarah Jane Owens <sarahjowens6@gmail.com>
2017-05-31 22:25:51 +0100

    portfolio changes

---
+++ js/easyPieChart.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('.chart').easyPieChart({
+    scaleColor: "#ecf0f1",
+    lineWidth: 15,
+    lineCap: 'butt',
+    barColor: '#6CB670',
+    trackColor:	"#ecf0f1",
+    size: 160,
+    animate: 900
+  });
+});
